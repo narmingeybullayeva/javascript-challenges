f70562c5c4605efc251633648836871265deff82
narmingeybullayeva <narmin.g.mammadli@gmail.com>
2024-05-14 13:24:29 +0400

    calculator challenges solved

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,5 +1,38 @@
-function calculator(num1, num2, operator) {
-  return num1 + operator + num2
+// function calculator(a, b, operator) {
+//   switch (operator) {
+//     case "+":
+//       result = a + b
+//       break
+//     case "-":
+//       result = a - b
+//       break
+//     case "*":
+//       result = a * b
+//       break
+//     case "/":
+//       result = a / b
+//       break
+//     default:
+//       throw new Error("Invalid operator")
+//   }
+//   return result
+// }
+
+function calculator(a, b, operator) {
+  if (operator == "+") {
+    return a + b
+  }
+  if (operator == "-") {
+    return a - b
+  }
+  if (operator == "*") {
+    return a * b
+  }
+  if (operator == "/") {
+    return a / b
+  } else {
+    throw new Error("Invalid operator")
+  }
 }
 
 module.exports = calculator
