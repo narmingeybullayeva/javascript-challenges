c8e996e03a557fd7901510bdfcb59a240c70ca9b
narmingeybullayeva <narmin.g.mammadli@gmail.com>
2024-05-17 17:31:22 +0400

    find maximum number challenge solved

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,18 @@
+// solution 1
+
+function findMaxNumber(arr) {
+  let max_num = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max_num) {
+      max_num = arr[i]
+    }
+  }
+
+  return max_num
+}
+
+// solution 2
+
 function findMaxNumber(arr) {
   return arr.sort((a, b) => a - b).at(-1)
 }
