f28bf299ad168548568e368e3a4fc3dd8bb56ced
narmingeybullayeva <narmin.g.mammadli@gmail.com>
2024-05-17 17:16:15 +0400

    count-occurrences challenge solved

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,15 @@
-function countOccurrences() {}
+function countOccurrences(word, letter) {
+  let count = 0
+  let new_word = word.toLowerCase()
+  for (let i = 0; i < new_word.length; i++) {
+    if (word[i] === letter) {
+      count++
+    }
+  }
 
-module.exports = countOccurrences;
+  return count
+}
+
+console.log(countOccurrences("hello", "y"))
+
+module.exports = countOccurrences
